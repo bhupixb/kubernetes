f8ccfdb92ab7f1f399cb4cb628552d036fc3eaf6
Bhupendra Yadav <30554307+bhupixb@users.noreply.github.com>
2022-11-12 17:15:09 +0530

    K8s Host Health Check from inside a Pod

---
+++ k8s-healthcheck-from-pod.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+K8S=https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT
+TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+CACERT=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
+
+curl -H "Authorization: Bearer $TOKEN" --cacert $CACERT $K8S/healthz
